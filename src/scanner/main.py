4c81b08e20d1499736554abff174a608a450b2a4
Nitro <ppoultonhaselier@gmail.com>
2026-02-27 16:20:34 +0000

    Updates main and Security Scanner class
    Added visit_Call to look at function calls within the code specifically lookinf for os.system(). When found a message is printed

--- src/scanner/main.py
+++ src/scanner/main.py
@@ -1,5 +1,26 @@
 import ast
 
 class SecurityScanner(ast.NodeVisitor):
-    pass
+    
+    def visit_Call(self, node):
+        
+        #Start checking for os function calls
+        if isinstance(node.func, ast.Attribute):
+            #Checking for os.system()
+            if getattr(node.func.value, "id", None) == "os" and node.func.attr == "system":
+                print("os.system() within code, this could be a secuirty vunerlability")
 
+dummyCode = """
+import os
+
+def backup_data(folder_name):
+    # DANGER: User input is passed directly to the shell
+    os.system("cp -r " + folder_name + " /backup")
+    print("Backup complete!")
+
+backup_data("my_files")
+"""
+
+parsedCode = ast.parse(dummyCode)
+Scanner = SecurityScanner()
+Scanner.visit(parsedCode)
