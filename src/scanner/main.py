867d6dc51d8ca84a50a6d98ce334cef1d56d8271
Nitro <ppoultonhaselier@gmail.com>
2026-03-02 14:22:11 +0000

    Edit to main to yield real time errors
    Changed the main code to return each error as they are found providing a visual representation of the software working.

--- src/scanner/main.py
+++ src/scanner/main.py
@@ -2,13 +2,29 @@ import ast
 
 class SecurityScanner(ast.NodeVisitor):
     
+    def scan(self, tree):
+        #Walks through the tree and yeilds reports one by one
+        for node in ast.walk(tree):
+            
+            result = self.visit(node)
+            if result:
+                yield from result
+
+    def report(self, node, message):
+
+        output = f"[!]Line {node.lineno}: {message}"
+        yield output
+
     def visit_Call(self, node):
         #Cheacking for atrribute function calls
         if isinstance(node.func, ast.Attribute):
         
             #Checking for os.system()
             if getattr(node.func.value, "id", None) == "os" and node.func.attr == "system":
-                print("os.system() within code, this could be a security vunerlability")
+                yield from self.report(node, "os.system() detected (Possible Command Injection).")
+            #Checking for shelx.quote()
+            if getattr(node.func.value, "id", None) == "shlex" and node.func.attr == "quote":
+                yield from self.report(node, "shlex.quote() detected.")
 
             #Checking for subprocess
             if getattr(node.func.value, "id", None) == "subprocess":
@@ -19,11 +35,11 @@ class SecurityScanner(ast.NodeVisitor):
                     valueUnparsed = ast.unparse(kw.value)
 
                     if keyWord == "shell" and valueUnparsed == "True":
-                        print("Subprocess function cointaining shell=True found.This could be a security vunerlability")
+                        yield from self.report(node, "subprocess with shell=True detected.")
                     
         #Checking for stand alone function calls
         if isinstance(node.func, ast.Name):
 
             #Checking eval()
-            if node.func.id == "eval" or node.func.id =="exec":
-                print(f"An {node.func.id}() function call is included in the code. This could be a security vunerlability")
+            if node.func.id == "eval" or node.func.id =="exec" or node.func.id == "compile":
+                yield from self.report(node, f"{node.func.id}() detected (Possible Code Injection).")
